711879aa2f911e447ded646ab7629b2ff1c20b53
Helton <heltoname@gmail.com>
2024-09-13 09:59:48 -0400

    Feat: adicionando pastas e arquivos atualizados

---
+++ include/footer.php
@@ -0,0 +1,11 @@
+<footer class="art-footer">
+  <div class="art-footer-inner">
+<div style="position:relative;display:inline-block;padding-left:42px;padding-right:42px"><a title="SGBIT" class="" style="position: absolute; bottom: -10px; left: -6px; line-height: 32px;" href="#"></a><p>&copy; <?php echo Date("Y"); ?> - by Helton Rodrigues.</p></div>
+    <p class="art-page-footer">
+        <span id="art-footnote-links"><a href="https://www.linkedin.com/in/heltonpinherodasilvarodrigues" target="_blank">https://www.linkedin.com/in/heltonpinherodasilvarodrigues</a> Linkendin.</span>
+    </p>
+  </div>
+</footer>
+
+</body>
+</html>
